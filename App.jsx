d4fe7767fffd97316076bd84b0372fcdb9a86e6f
Garvit1703 <121950129+Garvit1703@users.noreply.github.com>
2025-11-15 10:31:09 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,9 @@
+
+export default function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Your React App is Running!</h1>
+      <p>Now deploy this project on Vercel without any errors.</p>
+    </div>
+  );
+}
